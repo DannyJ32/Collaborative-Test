e2620534d6a38cd360f0ffdcc212f5f3dd69e5f7
BT_C09_08 <mobileappdev@bths.edu>
2026-04-23 13:59:39 -0400

    Edited Files

---
+++ collaborationfun/debug.swift
@@ -0,0 +1,18 @@
+//
+//  debug.swift
+//  collaborationfun
+//
+//  Created by Student on 4/23/26.
+//
+
+import SwiftUI
+
+struct debug: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    debug()
+}
