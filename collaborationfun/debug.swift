a49d36bcb88c13c51604f55e2696348e0a1dd782
BT_C09_08 <mobileappdev@bths.edu>
2026-04-24 13:34:55 -0400

    Changes to debug file

--- collaborationfun/debug.swift
+++ collaborationfun/debug.swift
@@ -8,8 +8,18 @@
 import SwiftUI
 
 struct debug: View {
+    @State private var count = 0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 10){
+            Text("Debug Dashboard")
+                .font(.headline)
+            HStack {
+                Text("Counter: \(count)")
+                Button("Increment") {
+                    count += 1
+                }
+            }
+        }
     }
 }
 
